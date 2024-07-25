3a7dd63d11cbf85a1d1131d034a242ee39207e13
Ryan <rkueter@gmail.com>
2024-07-24 20:15:44 -0500

    Updating repository.

--- _content/RTBlazorfied/js/RTBlazorfied.js
+++ _content/RTBlazorfied/js/RTBlazorfied.js
@@ -276,6 +276,10 @@ class RTBlazorfied {
                 this.getCode();
             }            
         }
+        if (event.ctrlKey && event.shiftKey && event.key === '&') {
+            event.preventDefault();
+            this.uploadImageDialog();
+        }
         if (event.ctrlKey && event.shiftKey && event.key === 'U') {
             event.preventDefault();
             this.unorderedlist();
@@ -1336,14 +1340,18 @@ class RTBlazorfiedNodeManager {
         }
     }
 
-    isExcluded = (selection) => {       
-        const table = selection.anchorNode.querySelector('table');
-        if (table != null) {
-            if (selection.anchorNode.parentNode !== this.content && selection.anchorNode.parentNode.nodeName !== 'TABLE') {
-                return true;
+    isExcluded = (selection) => {
+        /* Make certain the user cannot select table
+        cells from the left */
+        if (selection.anchorNode.nodeType === Node.ELEMENT_NODE) {
+            const table = selection.anchorNode.querySelector('table');
+            if (table != null) {
+                if (selection.toString().trim() !== table.innerText.trim()) {
+                    return true;
+                }
             }
         }
-
+        
         if (selection.anchorNode.parentNode !== this.content) {
             switch (selection.anchorNode.parentNode.nodeName) {
                 case "TD":
